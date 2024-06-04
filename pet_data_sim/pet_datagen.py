1e06c4a490b132f610747322fdfdf4d5d3ebbce6
morinae <168416271+morinae@users.noreply.github.com>
2024-06-04 15:47:59 +0200

    Update pet_datagen.py

--- pet_data_sim/pet_datagen.py
+++ pet_data_sim/pet_datagen.py
@@ -8,7 +8,7 @@ for pf in ["tacs_AD.h5", "tacs_C.h5"]:
 for pf in ["tacs_AD.npz", "tacs_C.npz"]:
 	for sc in [0.1, 1.0, 10.0]:
 		for frm in list(range(25)):
-			osem.main(["--phantom_file",pf,"--scanner_sens",sc,"--frm",frm])
+			osem.main(["--phantom_file",pf,"--num_seeds", str(20),"--scanner_sens", str(sc), "--frm",str(frm)])
 			
 flagfig = False
 
