7d89c9b0e035bbcec14caaa507abc71af86baf19
morinae <168416271+morinae@users.noreply.github.com>
2024-06-04 14:21:38 +0200

    Update pet_datagen.py

--- pet_data_sim/pet_datagen.py
+++ pet_data_sim/pet_datagen.py
@@ -6,7 +6,7 @@ for pf in ["tacs_AD.h5", "tacs_C.h5"]:
 	sim_data.main(["--phantom_file",pf])
 	
 for pf in ["tacs_AD.npz", "tacs_C.npz"]:
-	for sc in [0.1, 1.0, 10.0]
+	for sc in [0.1, 1.0, 10.0]:
 		for frm in list(range(25)):
 			osem.main(["--phantom_file",pf,"--scanner_sens",sc,"--frm",frm])
 			
